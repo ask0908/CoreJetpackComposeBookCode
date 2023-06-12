3525926e96db9f6ad4cdea11c6105851fa170393
ask0908 <askdfjaslk@naver.com>
2023-06-13 01:57:47 +0900

    41장
    뷰모델 연습2

--- app/src/main/java/com/example/composebookexample/ViewModelActivity.kt
+++ app/src/main/java/com/example/composebookexample/ViewModelActivity.kt
@@ -3,23 +3,22 @@ package com.example.composebookexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -34,48 +33,114 @@ class ViewModelActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TopLevel()
+                    ScreenSetup()
                 }
             }
         }
     }
 }
 
-class MyViewModel: ViewModel() {
-    var customerName: MutableLiveData<String> = MutableLiveData("")
-
-    fun setName(name: String) {
-        customerName.value = name
-    }
-}
-
 @Composable
-fun TopLevel(model: MyViewModel = MyViewModel()) {
-    val customerName: String by model.customerName.observeAsState("")
+fun ScreenSetup(viewModel: DemoViewModel = DemoViewModel()) {
+    MainScreen7(
+        isFahrenheit = viewModel.isFahrenheit,
+        result = viewModel.result,
+        convertTemp = { viewModel.convertTemp(it) },
+        switchChange = { viewModel.switchChange() }
+    )
 }
 
 @Composable
 fun MainScreen7(
-    count: Int,
-    addCount: () -> Unit = {}
+    isFahrenheit: Boolean,
+    result: String,
+    convertTemp: (String) -> Unit,
+    switchChange: () -> Unit
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxSize()
     ) {
+        var textState by remember { mutableStateOf("") }
+        val onTextChange = { text: String ->
+            textState = text
+        }
+
         Text(
-            modifier = Modifier.padding(10.dp),
-            text = "Total customers = $count"
+            modifier = Modifier.padding(20.dp),
+            text = "온도 바꾸기",
+            style = MaterialTheme.typography.h4
         )
-        Button(onClick = addCount) {
-            Text("Add a customer")
+        InputRow(
+            isFahrenheit = isFahrenheit,
+            textState = textState,
+            switchChange = switchChange,
+            onTextChange = onTextChange
+        )
+        Text(
+            modifier = Modifier.padding(20.dp),
+            text = result,
+            style = MaterialTheme.typography.h3
+        )
+
+        Button(onClick = { convertTemp(textState) }) {
+            Text("온도 변환")
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview14() {
+fun InputRow(
+    isFahrenheit: Boolean,
+    textState: String,
+    switchChange: () -> Unit,
+    onTextChange: (String) -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Switch(checked = isFahrenheit, onCheckedChange = { switchChange() })
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { onTextChange(it) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            singleLine = true,
+            label = { Text("온도 입력") },
+            modifier = Modifier.padding(10.dp),
+            textStyle = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_ac_unit_24),
+                    contentDescription = "frost",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        )
+    }
+
+    Crossfade(
+        targetState = isFahrenheit,
+        animationSpec = tween(2000)
+    ) { visible ->
+        when (visible) {
+            true -> Text("\u2109", style = MaterialTheme.typography.h4)
+            false -> Text("\u2103", style = MaterialTheme.typography.h4)
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview14(model: DemoViewModel = DemoViewModel()) {
     ComposeBookExampleTheme {
+        MainScreen7(
+            isFahrenheit = model.isFahrenheit,
+            result = model.result,
+            convertTemp = { model.convertTemp(it) },
+            switchChange = { model.switchChange() }
+        )
     }
 }
